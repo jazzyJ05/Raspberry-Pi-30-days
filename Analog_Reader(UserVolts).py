44a025f7bb05b7a93e47a98792d925d7b556c1cc
Jazzy <jazlinejeanfarah05@gmail.com>
2025-12-25 21:25:59 -0500

    Implement PWM control for voltage output

---
+++ Analog_Reader(UserVolts).py
@@ -0,0 +1,15 @@
+from machine import PWN, Pin
+from time import sleep
+
+outPin = 16
+analogOut = PWM(Pin(outPin))
+
+analogOut.Freq(1000)
+analogOut.duty_u16(0)
+
+while True:
+	myVoltage = float(input(‘What Voltage would you like’))
+	pwmVal = (65550/3.3) * myVoltage
+	analogOut.duty_u16(int(pwmVal))
+	print(pwnVal)
+	sleep(0.1)
